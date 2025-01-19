5e68de759687e9d8be9dea8e9582c439a464c027
Igor Kindop <Igor.Kindop@gmail.com>
2025-01-19 14:25:57 +0100

    MA simulation added

--- AR_classics.py
+++ AR_classics.py
@@ -22,7 +22,7 @@ def simple_ar_1(pars, N):
     """
        Simple AR process with one root and a constant.
         Inputs:
-        pars - constant and an autoregressive parameter
+        pars: List[float] - constant and an autoregressive parameter
         N - length of the process
         Returns:
         process - simulated AR process
@@ -34,18 +34,21 @@ def simple_ar_1(pars, N):
         simulated[item] = const + pars*simulated[item-1] + noise[item]
     return simulated
 
-def simulate_ar(pars, N):
-    """
-        A more general AR process with N roots and a constant.
-        Inputs:
-        pars - constant and an autoregressive parameter
-        N - length of the process
-        Returns:
-        process - simulated AR process
+def simulate_ar(pars, N, sigma = 1):
+    """Function to simulate an AR(p) process given the inputs. 
+    Returns an array,
+
+   Args:
+        pars (List[float]): a parameter list with a constant
+        N (int): length of the simulated process
+        sigma (int, optional): variance of the noise. Defaults to 1.
+
+    Returns:
+        Array: a simulated AR process.
     """
     const = pars.pop(0)
     M = len(pars)
-    noise = np.random.randn(N)
+    noise = np.random.randn(N)*abs(sigma)
     simulated = const + noise.copy()
     for item in range(1,N):
         if item < M:
@@ -54,5 +57,26 @@ def simulate_ar(pars, N):
             simulated[item] = const + np.dot(pars, simulated[(item-M):item]) + noise[item]
     return simulated
     
-    
-# test
+def simulate_ma(pars, N, sigma=1):
+    """Function to simulate an MA(q) process given the inputs. 
+    Returns an array,
+
+    Args:
+        pars (List[float]): a parameter list with a constant
+        N (int): length of the simulated process
+        sigma (int, optional): variance of the noise. Defaults to 1.
+
+    Returns:
+        Array: a simulated MA process.
+    """
+    const = pars.pop(0)
+    M = len(pars)
+    noise = np.random.randn(N)*abs(sigma)
+    simulated = const + noise.copy()
+    for item in range(1,N):
+        if item < M:
+            simulated[item] = const + np.dot(pars[:item], noise[:item]) + noise[item]
+        else:
+            simulated[item] = const + np.dot(pars, noise[(item-M):item]) + noise[item]
+    return simulated
+
